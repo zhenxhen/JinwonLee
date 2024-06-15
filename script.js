78cd1bccfdcf2e2ad15f7902cb707c2cfcd8e8f1
zhenxhen <96387936+zhenxhen@users.noreply.github.com>
2024-06-15 17:43:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.getElementById('clip-button').onclick = function() {
     button.classList.add('shake');
     setTimeout(function() {
         button.classList.remove('shake');
-    }, 820); // 애니메이션 지속 시간과 동일하게 설정
+    }, 820);
 };
+
